c3e0af23390a30ffdde0fb579ea034f451ff1af5
Erwin Atuli <erwin@promotexter.com>
2015-06-23 10:14:31 +0800

    initial import

---
+++ logger.js
@@ -0,0 +1,205 @@
+(function() {
+	"use strict";
+
+	var winston = require('winston');
+	var os      = require('os');
+	var MongoDB = require('winston-mongodb').MongoDB;
+	var SNS     = require('winston-sns');
+	var http    = require('http');
+	var crypto  = require('crypto');
+
+	var levels  = {
+		default_logger: {
+			levels: {
+				raw      : 0,
+				debug    : 1,
+				info     : 2,
+				warn     : 3,
+				error    : 4,
+				critical : 5
+			},
+			colors: {
+				raw      : 'grey',
+				debug    : 'inverse',
+				info     : 'green',
+				warn     : 'magenta',
+				error    : 'red',
+				critical : 'redBG',
+			}
+		},
+		raw_logger: {
+			levels: {
+				raw : 0,
+			},
+			colors: {
+				raw : 'grey',
+			}
+		}
+	};
+
+	//encapsulate winston logging
+	var Logger = function() {
+
+		var self      = this;
+		var args      = Array.prototype.slice.call(arguments);
+		self.hostname = os.hostname();
+		self.appname  = (args.length > 0) ? args[0] : null;
+
+
+		self.default_logger = new (winston.Logger)({level: 'raw',
+								levels: levels.default_logger.levels,
+								colors: levels.default_logger.colors});
+		self.raw_logger     = new (winston.Logger)({level: 'raw',
+								levels: levels.raw_logger.levels,
+								colors: levels.raw_logger.colors});
+		self.sns_logger = new (winston.Logger)({level: 'raw',
+								levels: levels.default_logger.levels,
+								colors: levels.default_logger.colors});
+
+
+		//options
+		var conf = require('rc')('logger', {});
+		self.conf = conf;
+
+		self.default_logger.add(winston.transports.Console, {'timestamp':true, 'colorize': true, 'level':'raw'});
+
+		if(typeof conf.file === 'object') {
+			conf.file.filename = (self.appname === null)? conf.file.filename : conf.file.filename +'-'+ self.appname.toLowerCase();
+			self.default_logger.add(winston.transports.DailyRotateFile, conf.file);
+			conf.file.filename += '-raw';
+			self.raw_logger.add(winston.transports.DailyRotateFile, conf.file);
+		}
+
+		if(typeof conf.mongodb === 'object') {
+			self.default_logger.add(MongoDB, conf.mongodb);
+		}
+
+		if(typeof conf.sns === 'object') {
+			self.sns_logger.add(SNS, conf.sns);
+		}
+
+
+		// winston supports string interpolation. We don't need that
+		// we just want to concat args
+
+		var sns_sent = {};
+		self.log = function(level, message) {
+
+			level    = level.toLowerCase();
+			var meta = {};
+			var more = Array.prototype.slice.call(arguments, 2);
+
+			meta.appname = self.appname;
+			meta.host    = self.hostname;
+
+			if(more.length > 0) {
+				for(var i in more) {
+					message = message + " " + JSON.stringify(more[i]);
+				}
+
+			}
+
+			if(level === 'raw') {
+				self.raw_logger.raw(message, meta);
+				self.default_logger[level.toLowerCase()](message);
+			} else {
+				self.default_logger[level.toLowerCase()](message);
+			}
+
+			if(level == 'error' || level == 'critical') {
+				self.log('debug', level.toUpperCase() + ':' + new Error().stack);
+			}
+
+			// sns logging
+			if(typeof conf.sns === 'object') {
+				if(levels.default_logger.levels[level] >= levels.default_logger.levels[conf.sns.level]) {
+
+					var hasher = crypto.createHash('md5');
+					var hash   = hasher.update(level + message).digest('hex');
+					var now    = Math.round(new Date().getTime() / 1000);
+
+					//log every t_since secs for identical messages
+					if(typeof sns_sent[hash] !== 'undefined') {
+						if(now - sns_sent[hash].t_since > conf.sns.interval) {
+							self.sns_logger[level.toLowerCase()](message, meta);
+							sns_sent[hash].t_since = now;
+						}
+					} else {
+						sns_sent[hash]         = {};
+						sns_sent[hash].t_since = now;
+						self.sns_logger[level.toLowerCase()](message, meta);
+					}
+				}
+			}
+		};
+
+		var methods = [
+			'raw',
+			'debug',
+			'info',
+			'warn',
+			'error',
+			'critical'
+		].forEach(function(e) {
+			self[e] = function() {
+				var args = [e];
+				args = args.concat(Array.prototype.slice.call(arguments));
+				self.log.apply(this, args);
+			};
+		});
+
+		self.profile = function(tag, msg) {
+			var meta     = {};
+			meta.appname = self.appname;
+			meta.host    = self.hostname;
+
+			if(typeof msg === 'undefined') {
+				msg = tag;
+			}
+			self.default_logger.profile(tag, msg, meta, function() {
+				//console.log(arguments);
+			});
+		};
+
+		self.stats = function(obj) {
+			if(typeof conf.magnesium !== 'object') {
+				return;
+			}
+			conf = self.conf;
+			var http_options = {
+				method   : 'POST',
+				hostname : conf.magnesium.host,
+				path     : '/stats',
+				port     : conf.magnesium.port
+
+			};
+
+
+			var body = {
+				'api_key'    : conf.magnesium.api_key,
+				'api_secret' : conf.magnesium.api_secret,
+				'server'     : self.hostname,
+				'stats'      : obj
+			};
+
+
+			http_options.headers =  {
+				'Content-Type': 'application/json'
+			};
+
+			var req = http.request(http_options, function(res) {
+				self.info('saved stats to magnesium', String(res));
+			});
+
+			req.on('error', function(e) {
+				self.error("cannot send log stats", e);
+			});
+
+
+			req.write(JSON.stringify(body));
+			req.end();
+		};
+	};
+
+	module.exports = Logger;
+})();
---
+++ tests/logtest.js
@@ -0,0 +1,26 @@
+(function() {
+	"use strict";
+
+	var Logger = require('../logger');
+	var Log = new Logger('myapp');
+	Log.appname = 'myapp';
+
+	Log.profile('request');
+	Log.raw("this is raw", "this is raw", "slkfj");
+	Log.debug("this is a debug", "this is raw", "slkfj");
+	Log.info('hello world', 'x', 'y', [1,2,3]);
+	Log.warn('hello world', 'x', 'y', [1,2,3]);
+	Log.error('this is an error');
+	Log.critical("this is critical", "this is raw", "slkfj");
+
+	Log.profile('request', "request duration");
+	Log.info("this is another info", 2, 3, 4, 5);
+	Log.stats( {a: 'b', c: 'd'});
+
+
+	setInterval(function() {
+		Log.error('yes');
+	}, 3000);
+
+
+})();
